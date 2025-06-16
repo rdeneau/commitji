3523b5a259e5dba0bd6f67408c016ea9bfd24b26
Romain Deneau <rdeneau@yahoo.fr>
2025-06-17 00:18:35 +0200

    feat: SelectionPrompt, Stepper

---
+++ Commitji.Cli/Markup.fs
@@ -0,0 +1,18 @@
+﻿module Commitji.Cli.Markup
+
+open System
+
+let highlight (input: string) (text: string) =
+    match input.Length with
+    | 0 -> text
+    | n ->
+        match text.IndexOf(input, StringComparison.OrdinalIgnoreCase) with
+        | -1 -> text
+        | i ->
+            let before = text[.. i - 1]
+            let after = text[i + n ..]
+            $"{before}[grey19 on yellow]%s{input}[/]%s{after}"
+
+let kbd text = $"[white on grey][[%s{text}]][/]"
+
+let selection text = $"[bold green1]%s{text}[/]"
