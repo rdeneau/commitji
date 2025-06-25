499833870be06a57ac160d98ac6e9f22fd5577c4
Romain Deneau <rdeneau@yahoo.fr>
2025-06-25 21:28:03 +0200

    chore(ci): fix semantic release, add emoji in job names
    - remove 'git' plugin
    - add 'exec' plugin doing dotnet pack and nuget push

--- release.config.mjs
+++ release.config.mjs
@@ -3,4 +3,16 @@
  */
 export default {
     branches: ["main"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/exec",
+            {
+                "prepareCmd": "dotnet pack --configuration Release -p:PackageVersion=${nextRelease.version} -p:PackageReleaseNotes=\"${nextRelease.notes}\" --output ./nuget",
+                "publishCmd": "dotnet nuget push ./nuget/*.nupkg --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json --skip-duplicate"
+            }
+        ],
+        "@semantic-release/github"
+    ]
 };
