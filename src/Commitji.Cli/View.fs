25f5a33a514603799dff05a3ba3ec412627de0e5
Romain Deneau <rdeneau@yahoo.fr>
2025-07-08 16:15:52 +0200

    chore: 🧹 remove the TODO regarding live display, not working properly (see dedicated branch)

--- src/Commitji.Cli/View.fs
+++ src/Commitji.Cli/View.fs
@@ -267,7 +267,6 @@ let private handleKeyPress (keyInfo: ConsoleKeyInfo) (model: Model) dispatch =
     | _, (ConsoleModifiers.None | ConsoleModifiers.Shift), c -> dispatch (InputChanged $"%s{model.CurrentStep.Input}%c{c}")
     | _ -> ()
 
-// TODO 💡 Live display: https://spectreconsole.net/live/live-display
 let render model dispatch =
     AnsiConsole.Clear()
 
